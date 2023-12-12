46b2ed184ef3d9097048ad757e70cb48c3898981
Jakub Šlof <jakub.sloff@gmail.com>
2023-12-12 18:25:17 +0100

    nefunguje to lol

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,60 @@
 #include<driver/gpio.h>
 #include<iostream>
+#include<chrono>
+#include<thread>
+
 constexpr gpio_num_t g_ledRed = GPIO_NUM_45;
 constexpr gpio_num_t g_ledGreen = GPIO_NUM_17;
 constexpr gpio_num_t g_ledBlue = GPIO_NUM_46;
 constexpr gpio_num_t g_ledYellow = GPIO_NUM_15;
+constexpr gpio_num_t g_Button1 = GPIO_NUM_18;
+constexpr gpio_num_t g_Button2 = GPIO_NUM_16;
+constexpr gpio_num_t g_Button3 = GPIO_NUM_46;
+
 
 extern "C" void app_main(void) {
-    // Initialize the LED pin as a output
+    using namespace std::chrono_literals;
     gpio_config_t io_conf = {
         .pin_bit_mask =  (1ULL << g_ledRed) | (1ULL << g_ledGreen)|(1ULL << g_ledBlue)|(1ULL << g_ledYellow),
         .mode = GPIO_MODE_OUTPUT,  
-        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,  
+        };
+    gpio_config_t but_conf = {
+        .pin_bit_mask =  (1ULL << g_Button1) | (1ULL << g_Button2)|(1ULL << g_Button3),
+        .mode = GPIO_MODE_INPUT,  
+        .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE,  
         };
 
 
 gpio_config(&io_conf);
-gpio_num_t ledPins[] = {g_ledRed, g_ledGreen, g_ledBlue, g_ledYellow};
-for (int i = 0; i < sizeof(ledPins)/sizeof(ledPins[0]); i++) {
-    gpio_set_level(ledPins[i], 1);
+gpio_config(&but_conf);
+while (true)
+{
+    // Check if any of the buttons are pressed
+    
+    if(gpio_get_level(g_Button1)){
+        std::cout<<"Button 1 Pressed\n";
+        gpio_set_level(g_ledRed, 0);
+    }
+    if(gpio_get_level(g_Button2)){
+        gpio_set_level(g_ledBlue, 0);
+    }
+    if(gpio_get_level(g_Button3)){
+        gpio_set_level(g_ledYellow, 0);
+    }
+    else{
+        gpio_set_level(g_ledYellow, 1);
+        gpio_set_level(g_ledGreen, 1);
+        gpio_set_level(g_ledBlue, 1);
+    }
+    
 }
 
-std::cout<<"Hello World!"<<std::endl;
+
 
     }
 
